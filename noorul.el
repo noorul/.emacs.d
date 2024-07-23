1283f017d5e74a030136129f3a9f4e1aa7b91774
Noorul Islam K M <noorul@noorul.com>
2024-07-23 23:25:24 +0530

    Add nix mode

--- noorul.el
+++ noorul.el
@@ -2869,6 +2869,18 @@ Late deadlines first, then scheduled, then non-late deadlines"
 (setq nxml-child-indent 4
       nxml-attribute-indent 4)
 
+(use-package nix-mode
+  :mode ("\\.nix\\'" "\\.nix.in\\'"))
+(use-package nix-drv-mode
+  :ensure nix-mode
+  :mode "\\.drv\\'")
+(use-package nix-shell
+  :ensure nix-mode
+  :commands (nix-shell-unpack nix-shell-configure nix-shell-build))
+(use-package nix-repl
+  :ensure nix-mode
+  :commands (nix-repl))
+
 (use-package keytar
   :after lsp-grammarly)
 
